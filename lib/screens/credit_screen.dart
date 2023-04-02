1052ebfb4c69110185c695ec8aa091ec2f514d7f
nattawat <nattawat.wi@kkumail.com>
2023-04-02 14:44:18 +0700

    update

--- lib/screens/credit_screen.dart
+++ lib/screens/credit_screen.dart
@@ -19,11 +19,12 @@ class _CreditScreenState extends State<CreditScreen> {
           children: [
             const CircleAvatar(
               radius: 75,
-              backgroundImage: AssetImage("assets/images/profile.jpg"),
+              backgroundImage: NetworkImage(
+                  "https://scontent.fbkk5-3.fna.fbcdn.net/v/t39.30808-6/283917183_1915172192012669_129191124400808929_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEDu5p1JEsvhSnUCVyhL49U-8APmxhPsGL7wA-bGE-wYlvRZ485t9djRIEfAwHAl-1qrzUJbSyMWuLuaZvBbqgC&_nc_ohc=S8LFyVljdV8AX99LFge&_nc_ht=scontent.fbkk5-3.fna&oh=00_AfDfrZUx2AwtPJKM2wGNoLHVieCbcf1_Izg-dNDtFyPLjw&oe=642E7E04"),
             ),
             const SizedBox(height: 16),
             const Text(
-              "ChatGPT",
+              "Nattawat Wilai",
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
@@ -44,14 +45,14 @@ class _CreditScreenState extends State<CreditScreen> {
               children: [
                 IconButton(
                   onPressed: () {},
-                  icon: Image.asset("assets/images/twitter.png"),
-                  tooltip: "Twitter",
+                  icon: Icon(Icons.hub),
+                  tooltip: "Github",
                 ),
                 const SizedBox(width: 16),
                 IconButton(
                   onPressed: () {},
-                  icon: Image.asset("assets/images/github.png"),
-                  tooltip: "GitHub",
+                  icon: Icon(Icons.facebook),
+                  tooltip: "Facebook",
                 ),
               ],
             ),
